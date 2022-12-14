44aa7d0ee5489c73adfbcdf9744ad7aaaa7207ee
xxxRockxxx <ivanov.nikita09012001@yandex.ru>
2022-12-14 14:40:10 +0300

    Add Save
    Add Save(doesn't work)

--- SaveData.cs
+++ SaveData.cs
@@ -4,6 +4,7 @@ using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Windows.Forms;
 
 namespace YES
 {
@@ -11,13 +12,27 @@ namespace YES
     {
         public void WriteFileAsync()
         {
-            string text = "";
-            string path = @"C:\Users\USER\Desktop\test2.txt";
-            foreach (var data in Data.All_Periods)
+            SaveFileDialog saveFileDialog1 = new SaveFileDialog();
+            //string Data = "";
+
+            if (saveFileDialog1.ShowDialog() == DialogResult.OK)
             {
-                text+=($"{data.Key};{data.Value}\n");
+                RichTextBox.SaveFile(saveFileDialog1.FileName);
             }
-            File.WriteAllText(path, text);  
+            //string path = saveFileDialog1.FileName;
+            //string text = "";
+            //File.WriteAllText(path, text);
+            //saveFileDialog1.Filter = "Text files(*.txt)|*.txt|All files(*.*)|*.*";
+            //if (saveFileDialog1.ShowDialog() == DialogResult.Cancel)
+            //    return;
+            //string filename= saveFileDialog1.FileName;
+            //string text = "";
+            //string path = @"C:\Users\USER\Desktop\test2.txt";
+            //foreach (var data in Data.All_Periods)
+            //{
+            //    text+=($"{data.Key};{data.Value}\n");
+            //}
+            //File.WriteAllText(path, text);  
         }
     }
 }
